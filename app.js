3ea0c7623dd748d1c3dbc6f843a66031c11aa2b1
izzwerg <izzwerg.kr@gmail.com>
2024-05-06 22:22:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('Наше проміжне ПЗ');
+    next();
+   });
+
 app.get('/', (req, res) => {
  res.send('Hello World!');
 });
 
+
+
 app.listen(3000, () => {
  console.log('Example app listening on port 3000!');
 });
+
